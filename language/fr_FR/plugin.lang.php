3090e48ca18f4ac6f84817dc32ee530b7064257f
xbgmsharp <xbgmsharp@gmail.com>
2013-05-31 14:40:22 +0900

    Add basic French translation

---
+++ language/fr_FR/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+
+$lang['DISPLAY'] = "Display configuration";
+$lang['SHOWLOCATION'] = "Display location map";
+$lang['SHOWLOCATION_DESC'] = "Show a map on right panel on picture page";
+$lang['SHOWWORLDMAPLEFT'] = "Add a world map menu on the left";
+$lang['SHOWWORLDMAPLEFT_DESC'] = "Show a world map menu on the left menu, will display all the image of the gallery";
+
+$lang['R_MAP'] = "Right Panel configuration";
+$lang['ADD_BEFORE'] = "Add location after";
+$lang['ADD_BEFORE_DESC'] = "Where to display the location information";
+$lang['HEIGHT'] = "Hauteur de la carte";
+$lang['HEIGHT_DESC'] = "en pixel";
+$lang['ZOOM'] = "Niveau de Zoom";
+$lang['ZOOM_DESC'] = "1: pays, 18: quartier";
+$lang['LOCATION'] = "Location";
+
+$lang['G_MAP'] = "Configuration de la Carte";
+$lang['BASELAYER'] = "Style de Carte";
+$lang['BASELAYER_DESC'] = "Mapnik est le style par défaut d'OpenStreetMap.";
+$lang['CUSTOMBASELAYER'] = "Style de carte personnalisé";
+$lang['CUSTOMBASELAYER_DESC'] = "Seulement si vous avez sélectionné un style de carte 'personnalisé (custom)'. Qui fournit le style de carte?";
+$lang['CUSTOMBASELAYERURL'] = "URL serveur de Tuile";
+$lang['CUSTOMBASELAYERURL_DESC'] = "Seulement si vous avez sélectionné un style de carte 'personnalisé (custom)'. Exemple: http://tile.openstreetmap.org/{z}/{x}/{y}.png";
+$lang['NOWORLDWARP'] = "No Worldwarp";
+$lang['NOWORLDWARP_DESC'] = "Normalement, le monde continuera vers la gauche et la droite. Cette option limite la vue d'un 'monde unique'.";
+$lang['ATTRLEAFLET'] = "Affiche 'Powered by Leaflet'";
+$lang['ATTRLEAFLET_DESC'] = "Affiche l'attribution de la librairie JavaScript pour les cartes interactives'Leaflet'.";
+$lang['ATTRIMAGERY'] = "Affiche le style de carte";
+$lang['ATTRIMAGERY_DESC'] = "Affiche le style de carte (par défaut Mapnik).";
+$lang['ATTRPLUGIN'] = "Affiche le nom de l'auteur";
+$lang['ATTRMPLUGIN_DESC'] = "J'ai créé cette extension. :)";
+
+$lang['PLUGINCONF'] = "Synchronize option";
+$lang['AUTOSYNC'] = "Automatically sync EXIF if exist";
+$lang['AUTOSYNC_DESC'] = "When metadata is synchronized, coordinates might be overriden.";
+
+$lang['WORLDMAP']='World map';
+
+$lang['IMAGERYBY'] = "Images par";
+$lang['PLUGINBY'] = "Plugin créé par";
+?>
