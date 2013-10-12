b4b54e103224c2a9f0bd32e5836a757c456b9ecf
xbgmsharp <xbgmsharp@gmail.com>
2013-10-12 15:07:00 +0900

    Update/add new translation, thanks to the contributors

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,97 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NOWORLDWARP'] = 'Kein Weltkrümmung';
+$lang['SHOWWORLDMAPLEFT_DESC'] = 'Die Anzeige des Weltkartenmenüs im linke Menü zeigt alle Bilder in der Galerie.';
+$lang['POPUPTHUMB'] = 'Vorschaubild';
+$lang['POWERBY'] = 'Powered by';
+$lang['RIGHTLINK'] = 'Name des Orts-headers';
+$lang['RIGHTLINKCSS'] = 'Eigener CSS';
+$lang['RIGHTLINKCSS_DESC'] = 'Benutzerdefinierter CSS-Stil, der auf den Orts-header angewendet werden soll.';
+$lang['RIGHTLINK_DESC'] = 'Gib den Namen ein, der oben auf der Karte angezeigt werden soll.';
+$lang['R_MAP'] = 'Konfiguration des rechten Panels';
+$lang['SHOWLOCATION'] = 'Zeige Ortskarte an';
+$lang['SHOWLOCATION_DESC'] = 'Zeige eine Karte im rechten Panel der Bildseite an.';
+$lang['SHOWOSM'] = 'Stelle \'Zeige in OpenStreetMap\' dar';
+$lang['SHOWOSM_DESC'] = 'Füge nach der Karte eine Link hinzu, um sie in OpenStreetMap anzuzeigen.';
+$lang['SHOWWORLDMAPLEFT'] = 'Links ein Weltkartenmenü hinzufügen';
+$lang['ZOOM'] = 'Zoomniveau';
+$lang['ZOOM_DESC'] = '1: weit weg, 18: sehr nah';
+$lang['PINOFFSET'] = 'Pin-punktabstand';
+$lang['PINOFFSET_DESC'] = 'Die Position im Pinbild, wo der Pin auf die Karte trifft. 0x0 bezeichnet die obere, linke Ecke. Beispiel: Fadenkreuz = Bildmitte';
+$lang['PINPATH'] = 'Eigener Pin (benutzerdefinierter Stil)';
+$lang['PINPATH_DESC'] = 'Pfad zu deinem Pin (nur wenn \'eigener\' ausgewählt wurde).';
+$lang['PINPOPUPOFFSET'] = 'Popup-abstand';
+$lang['PINPOPUPOFFSET_DESC'] = 'Die Position im Pinbild, wo das Popup auf den Pin trifft. 0x0 ist der Pin-punkt.';
+$lang['PINSHADOWPATH'] = 'Schatten';
+$lang['PINSHADOWPATH_DESC'] = 'Pfad zum Schattenbild deines Pins.';
+$lang['PINSHADOWSIZE'] = 'Schattengröße';
+$lang['PINSHADOWSIZE_DESC'] = 'Größe des Bildes in Pixel. Verwende \'x\' als Trennzeichen. Beispiel: \'24x24\'';
+$lang['PINSIZE'] = 'Pingröße';
+$lang['PINSIZE_DESC'] = 'Größe des Bildes in Pixel. Verwende \'x\' als Trennzeichen. Beispiel: \'24x24\'';
+$lang['PIN_DESC'] = 'Wähle die letzte Option aus, wenn du deinen Eigenen benutzen willst.';
+$lang['PLUGINBY'] = 'Plugin von';
+$lang['PLUGINCONF'] = 'Pluginoptionen';
+$lang['POPUPAUTHOR'] = 'Urheber des Bildes';
+$lang['POPUPCOMMENT'] = 'Bildkommentar';
+$lang['POPUPLINK'] = 'Einen Link zum Bild hinzufügen';
+$lang['POPUPNAME'] = 'Bildname';
+$lang['DEFAULTPIN'] = 'standardmäßig';
+$lang['DISPLAY'] = 'Anzeigekonfiguration';
+$lang['G_MAP'] = 'Kartenkonfiguration';
+$lang['HEIGHT'] = 'Kartenhöhe';
+$lang['HEIGHT_DESC'] = 'in Pixel';
+$lang['H_PIN'] = 'Pinkonfiguration';
+$lang['IMAGERYBY'] = 'Bilder von';
+$lang['LEFTLINK'] = 'Name des Weltkartenmenüs';
+$lang['LEFTLINK_DESC'] = 'Gib den Namen des Links an, der im linken Menü angezeigt werden soll.';
+$lang['LEFTPOPUP'] = 'Popup anzeigen';
+$lang['LEFTPOPUPINFO'] = 'Den Popupinhalt auswählen';
+$lang['LEFTPOPUPINFO_DESC'] = 'Wähle aus, was im Popup angezeigt werden soll';
+$lang['LEFTPOPUP_DESC'] = 'Ein Popupfenster anzeigen, wenn du auf den Pin klickst.';
+$lang['L_MAP'] = 'Konfiguration des linken Menüs';
+$lang['NEVER'] = 'kein Popup';
+$lang['NOPIN'] = 'kein Pin';
+$lang['NOWORLDWARP_DESC'] = 'Normalerweise kann links und rechts kontinuierlich durch die Welt gescrolled werden. Dies wird durch diese Option unterbunden.';
+$lang['OWNPIN'] = 'Eigener (benutzerdefinierter Stil)';
+$lang['PIN'] = 'Pin auswählen';
+$lang['BASELAYER_DESC'] = 'Mapnik ist der Standardstil bei Openstreetmap.';
+$lang['BATCHMANAGER'] = 'Integration in den Batch-manager aktivieren';
+$lang['BATCHMANAGER_DESC'] = 'Manuelles Anpassen von Längen- und Breitengraden erlauben. Deaktiviere diese Funktion, wenn \'RV Maps & Earth\' installiert ist.';
+$lang['CLICK'] = 'bei Click anzeigen';
+$lang['CUSTOMBASELAYER'] = 'Benutzerdefinierter Kartenstil';
+$lang['CUSTOMBASELAYERURL'] = 'URL für Tile-server';
+$lang['CUSTOMBASELAYERURL_DESC'] = 'Nur wenn \'benutzerdefiniert\' als Kartenstil ausgewählt ist. Zum Beispiel: http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+$lang['CUSTOMBASELAYER_DESC'] = 'Nur wenn \'benutzerdefiniert\' als Kartenstil ausgewählt ist. Wer liefert den Kartenstil (Tiles)?';
+$lang['BASELAYER'] = 'Kartenstil';
+$lang['AUTOSYNC_DESC'] = 'Wenn Metadaten synchronisiert werden, könnten Koordinaten überschrieben werden. Deaktiviere diese Funktion, wenn \'RV Maps & Earth\' installiert ist.';
+$lang['AUTOSYNC'] = 'EXIF Daten automatisch synchronisieren, wenn sie vorgefunden werden';
+$lang['ATTRPLUGIN_DESC'] = 'Ich habe diesen Plugin erstellt. :)';
+$lang['ATTRPLUGIN'] = 'Notiz des Autors anzeigen';
+$lang['ATTRLEAFLET_DESC'] = 'Zuordnung der benutzten Javascript Bibliothek für interaktive \'Leaflet\' Karten anzeigen.';
+$lang['ATTRLEAFLET'] = '\'Powered by Leaflet\' anzeigen';
+$lang['ATTRIMAGERY_DESC'] = 'Den benutzten Kartenstil anzeigen.';
+$lang['ATTRIMAGERY'] = 'Kartenstil anzeigen';
+$lang['ALWAYS'] = 'sofort anzeigen';
+$lang['ADD_BEFORE_DESC'] = 'Wo die Ortsinformation angezeigt werden soll.';
+$lang['ADD_BEFORE'] = 'Ort vor hinzufügen';
+?>
