69b257b498538529273a6d3648cecd8f9f8f98fb
xbgmsharp <xbgmsharp@gmail.com>
2013-10-19 16:44:05 +0900

    Update/add new translation, thanks to the contributors

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,97 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['AUTOSYNC_DESC'] = 'Kiedy metadane są zsynchronizowane, koordynaty mogą zostać odrzucone. Wyłącz jeśli masz zainstalowane \'RV Maps&Earth\'';
+$lang['IMAGERYBY'] = 'Imagery by';
+$lang['POPUPCOMMENT'] = 'Komentarz zdjęcia';
+$lang['POPUPLINK'] = 'Dodaj link do zdjęcia';
+$lang['POPUPNAME'] = 'Nazwa zdjęcia';
+$lang['POPUPTHUMB'] = 'Miniaturka zdjęcia';
+$lang['POWERBY'] = 'Powered by';
+$lang['RIGHTLINK'] = 'Nazwa nagłówka lokalizacji';
+$lang['RIGHTLINKCSS'] = 'Osobisty CSS';
+$lang['RIGHTLINKCSS_DESC'] = 'Styl osobistego CSS do zastosowania w nagłówku lokalizacji';
+$lang['RIGHTLINK_DESC'] = 'Wpisz nazwę do wyświetlania na górze mapy';
+$lang['R_MAP'] = 'Konfiguracja prawego panelu';
+$lang['SHOWLOCATION'] = 'Wyświetla mapę lokalizacji';
+$lang['SHOWLOCATION_DESC'] = 'Pokaż mapę na prawym panelu strony zdjęcia';
+$lang['SHOWOSM'] = 'Wyświetla \'Zobacz na OpenStreetMap\'';
+$lang['SHOWWORLDMAPLEFT'] = 'Dodaj menu mapy po lewej stronie';
+$lang['SHOWWORLDMAPLEFT_DESC'] = 'Pokazuje menu mapy na lewym menu, będzie wyswietlał wszystkie zdjęcia w galerii.';
+$lang['ZOOM'] = 'Poziom Zoomu';
+$lang['ZOOM_DESC'] = '1: bardzo daleko, 18: bardzo blisko';
+$lang['OWNPIN'] = 'własny (osobisty styl)';
+$lang['PIN'] = 'Wybierz pinezkę';
+$lang['PINOFFSET'] = 'Tło punktu pinezki';
+$lang['PINOFFSET_DESC'] = 'Pozycja obrazka pinezki, miejsce gdzie pinezka jest wbita w mapę.';
+$lang['PINPATH'] = 'Własna pinezka (osobisty styl)';
+$lang['PINPATH_DESC'] = 'Ściezka do pinezki (tylko jeśli wybrany jest \'własny\')';
+$lang['PINPOPUPOFFSET'] = 'Tło popup';
+$lang['PINPOPUPOFFSET_DESC'] = 'Pozycja na obrazku pinezki gdzie popup dotyka pinezki.';
+$lang['PINSHADOWPATH'] = 'Cień';
+$lang['PINSHADOWPATH_DESC'] = 'Ścieżka do obrazka z cieniem Twojej pinezki';
+$lang['PINSHADOWSIZE'] = 'Rozmiar cienia';
+$lang['PINSHADOWSIZE_DESC'] = 'Wymiary obrazka w pixelach. Znakiem podziału jest \'x\'. Np: \'24x24\'';
+$lang['PINSIZE'] = 'Rozmiar pinezki';
+$lang['PINSIZE_DESC'] = 'Wymiary obrazka w pixelach. Znakiem podziału jest \'x\'. Np: \'24x24\'';
+$lang['PIN_DESC'] = 'Wybierz ostatnią opcję jeśli chcesz używać własnej.';
+$lang['PLUGINBY'] = 'Wtyczka';
+$lang['PLUGINCONF'] = 'Opcje wtyczki';
+$lang['POPUPAUTHOR'] = 'Autor obrazka';
+$lang['ATTRPLUGIN'] = 'Pokaż adnotację od autora';
+$lang['DEFAULTPIN'] = 'domyślne';
+$lang['DISPLAY'] = 'Wyświetl konfigurację';
+$lang['G_MAP'] = 'Mapa konfiguracji';
+$lang['HEIGHT'] = 'Wysokość mapy';
+$lang['HEIGHT_DESC'] = 'w pikselach';
+$lang['H_PIN'] = 'Konfiguracja pinezki';
+$lang['LEFTLINK'] = 'Nazwa menu mapy świata';
+$lang['LEFTLINK_DESC'] = 'Wpisz nazwę linka pokazywaną w lewym menu';
+$lang['LEFTPOPUP'] = 'Pokaż popup';
+$lang['LEFTPOPUPINFO'] = 'Wybierz zawartość popup';
+$lang['LEFTPOPUPINFO_DESC'] = 'Wybierz co wyświetlać w popup\'ie';
+$lang['LEFTPOPUP_DESC'] = 'Pokaż okno popup po kliknięciu na pinezkę.';
+$lang['L_MAP'] = 'Konfiguracja lewego menu';
+$lang['NEVER'] = 'brak popup\'u';
+$lang['NOPIN'] = 'brak pinezki';
+$lang['NOWORLDWARP'] = 'Brak Worldwarp';
+$lang['NOWORLDWARP_DESC'] = 'Zwykle świat będzie wyświetlany od lewa do prawa. Ta opcja pozwala na wyświetlanie tylko \'jednego świata\'';
+$lang['ADD_BEFORE'] = 'Wcześniej dodaj lokalizację';
+$lang['ADD_BEFORE_DESC'] = 'Gdzie wyświetlać informację o lokalizacji';
+$lang['ALWAYS'] = 'Pokaż natychmiast';
+$lang['ATTRIMAGERY'] = 'Pokaż styl mapy';
+$lang['ATTRIMAGERY_DESC'] = 'Pokazuje użyte style mapy';
+$lang['ATTRLEAFLET'] = 'Pokaż \'Powered by Leaflet\'';
+$lang['ATTRLEAFLET_DESC'] = 'Pokazuje a';
+$lang['ATTRPLUGIN_DESC'] = 'Ja stworzylem tą wtyczkę. :)';
+$lang['AUTOSYNC'] = 'Automatycznie synchronizuje EXIF jesli istnieje';
+$lang['BASELAYER'] = 'Styl mapy';
+$lang['BASELAYER_DESC'] = 'Mapnik jest domyślnym stylem OpenStreetMap';
+$lang['BATCHMANAGER'] = 'Uaktywnij integrację z Batch Managerem';
+$lang['BATCHMANAGER_DESC'] = 'Pozwala na manualną modyfikację szerokości i długości geograficznej. Deaktywuj jesli masz zainstalowany \'RV Maps & Earth\'';
+$lang['CLICK'] = 'pokaż po kliknięciu';
+$lang['CUSTOMBASELAYER'] = 'Osobisty styl mapy';
+$lang['CUSTOMBASELAYERURL'] = 'URL serwera tile';
+$lang['CUSTOMBASELAYERURL_DESC'] = 'Tylko jeśli jest wybrany \'osobisty\' styl mapy. Np: http://tile.openstreetmap.org/{z}/{x}/{y}.png';
+$lang['CUSTOMBASELAYER_DESC'] = 'Tylko jeśli jest wybrany \'osobisty\' styl mapy. Kto dostarczył styl mapy?';
+$lang['SHOWOSM_DESC'] = 'Dodaj pod mapą odnośnik do wyświetlenia w OpenStreetMap.';
+?>
