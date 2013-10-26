dbbe58648e30bfb8160ca15eef7ef6949e552ef4
xbgmsharp <xbgmsharp@gmail.com>
2013-10-26 16:18:50 +0900

    Update/add new translation, thanks to the contributors

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,97 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['RIGHTLINKCSS'] = 'Personalizar CSS';
+$lang['ZOOM'] = 'Nivel de zoom';
+$lang['PINPATH'] = 'Pino próprio ( personalizado)';
+$lang['PINSHADOWPATH'] = 'Sombra';
+$lang['PINSHADOWPATH_DESC'] = 'Caminho para a imagem sombra do pino.';
+$lang['PINSHADOWSIZE'] = 'Tamanho da sombra';
+$lang['PINSIZE'] = 'Tamanho do pino';
+$lang['PINSIZE_DESC'] = 'Dimensões da imagem em px. A delimitação é \'x\'. Exemplo: \'24x24\'';
+$lang['PIN_DESC'] = 'Selecione a ultima opção se deseja usar a própria';
+$lang['PLUGINBY'] = 'Extensão por';
+$lang['PLUGINCONF'] = 'Opções da extensão';
+$lang['POPUPAUTHOR'] = 'Autor da imagem';
+$lang['POPUPCOMMENT'] = 'Comentário da imagem';
+$lang['POPUPLINK'] = 'Adicionar um link para a imagem';
+$lang['POPUPNAME'] = 'Nome da imagem';
+$lang['POPUPTHUMB'] = 'Miniatura da imagem';
+$lang['DEFAULTPIN'] = 'Por defeito';
+$lang['DISPLAY'] = 'Mostrar configuração';
+$lang['G_MAP'] = 'Configuração do mapa';
+$lang['HEIGHT'] = 'Altura do mapa';
+$lang['HEIGHT_DESC'] = 'em px';
+$lang['H_PIN'] = 'Configuração do pin';
+$lang['IMAGERYBY'] = 'Imagens de';
+$lang['LEFTLINK'] = 'Nome do menú do mapa mundo';
+$lang['LEFTLINK_DESC'] = 'Tipo de nome do link para mostrar à esquerda do menú.';
+$lang['LEFTPOPUP'] = 'Mostar pop-up';
+$lang['LEFTPOPUPINFO'] = 'Selecionar o conteúdo do pop-up';
+$lang['LEFTPOPUPINFO_DESC'] = 'Escolher o que mostrar no pop-up';
+$lang['LEFTPOPUP_DESC'] = 'Mostrar uma janela pop-up se clicar sobre o pino.';
+$lang['L_MAP'] = 'Configuração do menú esquerdo';
+$lang['NEVER'] = 'sem pop-up';
+$lang['NOPIN'] = 'sem pino';
+$lang['NOWORLDWARP'] = 'Sem mapa mundo';
+$lang['NOWORLDWARP_DESC'] = 'Normalmente, o mundo continuará à esquerda e direita. Esta opção restringe a visão de "um mundo".';
+$lang['OWNPIN'] = 'proprio(estilo personalizado)';
+$lang['PIN'] = 'Selecionar pino';
+$lang['ADD_BEFORE'] = 'Adicionar localização antes';
+$lang['ADD_BEFORE_DESC'] = 'Onde mostrar a informação da localização';
+$lang['ALWAYS'] = 'Mostrar imediatamente';
+$lang['ATTRIMAGERY'] = 'Mostrar o estilo do mapa';
+$lang['ATTRIMAGERY_DESC'] = 'Mostras do estilo de mapa em uso';
+$lang['ATTRLEAFLET'] = 'Mostrar "Produzido por folheto"';
+$lang['ATTRLEAFLET_DESC'] = 'Mostra atribuição da biblioteca JavaScript utilizada para mapas "Folheto" interativos. ';
+$lang['ATTRPLUGIN'] = 'Mostrar nota do Autor';
+$lang['ATTRPLUGIN_DESC'] = 'Eu criei esta extensão. :)';
+$lang['AUTOSYNC'] = 'Sincronizar EXIF automáticamente se existir';
+$lang['AUTOSYNC_DESC'] = 'Quando os metadados são sincronizados, as coordenadas podem ser substituídas. Desativar extensão \'Mapas RV & Terra\' se tiver instalada. ';
+$lang['BASELAYER'] = 'Estilo de mapa';
+$lang['BASELAYER_DESC'] = 'Mapnik é o estilo padrão da OpenStreetMap.';
+$lang['BATCHMANAGER'] = 'Ativar a integração do gestor de carga';
+$lang['BATCHMANAGER_DESC'] = 'Permitir modificar latitude e longitude manualmente. Desativar \'Mapas RV & terra\' se a extensão estiver instalada.';
+$lang['CLICK'] = 'Mostrar ao clicar';
+$lang['CUSTOMBASELAYER'] = 'Personalizar o estilo do mapa';
+$lang['CUSTOMBASELAYERURL'] = 'URL do servidor de cobertura';
+$lang['CUSTOMBASELAYERURL_DESC'] = 'Apenas se \'personalizar\' está selecionado como mapa estilo. Exemplo: http://tile.openstreetmap.org/{z}/{x}/{y}png.';
+$lang['CUSTOMBASELAYER_DESC'] = 'Apenas se \'personalizar\' está selecionado como mapa estilo. Quem entrega o estilo de mapa (coberturas)?';
+$lang['PINOFFSET'] = 'Deslocamento do pino';
+$lang['PINPATH_DESC'] = 'Caminho para o seu pino ( apenas se está selecionado "próprio"';
+$lang['PINPOPUPOFFSET'] = 'delocamento do pop-up';
+$lang['PINSHADOWSIZE_DESC'] = 'Dimensões da foto em px. A delimitação é \'x\'. Exemplo: \'24x24\'';
+$lang['POWERBY'] = 'Realizado por';
+$lang['RIGHTLINK'] = 'Nome do cabeçalho local';
+$lang['RIGHTLINKCSS_DESC'] = 'Personalizar estilo CSS para aplicar ao cabeçalho local';
+$lang['R_MAP'] = 'Configuração do painel direito';
+$lang['SHOWLOCATION_DESC'] = 'Mostrar um mapa no painel direito da página da foto';
+$lang['PINOFFSET_DESC'] = 'A posição da imagem pino, onde o pino atinge o mapa. 0x0 é o canto superior esquerdo. Exemplo: mira = centro da imagem';
+$lang['PINPOPUPOFFSET_DESC'] = 'A posição do pino da imagem, onde a pop-up atinge o pino. 0x0 é o ponto de pino.';
+$lang['SHOWOSM'] = 'Mostrar \'Ver openStreetMap\'';
+$lang['SHOWOSM_DESC'] = 'Adicionar um link após o mapa para vê-lo no OpenStreetMap.';
+$lang['SHOWWORLDMAPLEFT_DESC'] = 'Mostrar um menu do mapa mundo no menu à esquerda, mostrará todas as imagens da galeria.';
+$lang['RIGHTLINK_DESC'] = 'Escreva o nome a mostrar no topo do mapa';
+$lang['SHOWLOCATION'] = 'Mostrar a localização do mapa';
+$lang['SHOWWORLDMAPLEFT'] = 'Adicionar à esquerda um menú do mapa mundial';
+$lang['ZOOM_DESC'] = '1: longe, 18: muito pertp';
+?>
