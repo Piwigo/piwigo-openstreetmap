a859deab481cbfaed8a4e88d91f7e7b733113194
xbgmsharp <xbgmsharp@gmail.com>
2014-11-06 18:56:50 +0900

    Neew option for WorldMap layout part2

--- maintain.inc.php
+++ maintain.inc.php
@@ -118,11 +118,11 @@ function plugin_install()
 	$c = <<<EOF
 <?php
 define('PHPWG_ROOT_PATH','./');
-if (isset($_GET['v']) and $_GET['v'] == 1)
+if (isset(\$_GET['v']) and \$_GET['v'] == 1)
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap.php');
-else if (isset($_GET['v']) and $_GET['v'] == 2)
+else if (isset(\$_GET['v']) and \$_GET['v'] == 2)
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap2.php');
-else if (isset($_GET['v']) and $_GET['v'] == 3)
+else if (isset(\$_GET['v']) and \$_GET['v'] == 3)
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap3.php');
 else
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap2.php');
