f419e8dd026ee9935a40f8f8366bf01e57a0fddd
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-15 23:16:12 +0100

    [piwigo_openstreetmap] Update uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['EDIT_UPDATE_LOCATION_DESC'] = 'Натисніть всередині мапи для оновлення місця розташування.';
+$lang['FIND_POSITION'] = 'Знайти свою позицію';
+$lang['G_MAP'] = 'Налаштування мапи';
+$lang['HEIGHT'] = 'Висота карти';
+$lang['EDIT_MAP'] = 'Мапа';
+$lang['COPY_PASTE_URL'] = 'Скопіюйте і вставте URL нижче:';
+$lang['CLICKED_MAP'] = 'Ви натиснули на мапу в';
+$lang['CENTER_MAP_DESC'] = 'Центрувати мапу світу на визначеній ділянці (GPS координати). Широта та довгота розділені комою, за замовченням 0,0';
+$lang['CUSTOMBASELAYERURL'] = 'URL серверу тайлів';
+$lang['CENTER_MAP'] = 'Центр мапи тут';
+$lang['BROWSER_JAVASCRIPT'] = 'Ваш браузер повинен мати активований JavaScript';
+$lang['BASELAYER_DESC'] = 'Mapnik це стиль за замовчуванням у OpenStreetMap.';
+$lang['BASELAYER'] = 'Стиль мапи';
+$lang['BACK_ON_PAGE'] = 'Повернутися назад на одну сторінку';
+$lang['ATTRLEAFLET_DESC'] = 'Показує спроможність використання бібліотеки JavaScript для інтерактивних карт «листівка».';
+$lang['ATTRLEAFLET'] = 'Показати бібліотечну примітку';
+$lang['ATTRPLUGIN'] = 'Показати примітку автора';
+$lang['ATTRIMAGERY_DESC'] = 'Показує використаний стиль мапи';
+$lang['ATTRIMAGERY'] = 'Показати стиль мапи';
+$lang['OSM_CONTRIBUTORS'] = '<a href="http://www.openstreetmap.org" target="_blank">OpenStreetMap</a> автори, (<a href="http://www.openstreetmap.org/copyright" target="_blank">ODbL</a>)';
+$lang['LATITUDE'] = 'Широта';
+$lang['M_MAP'] = 'Головне меню';
+$lang['LONGITUDE'] = 'Довгота';
+$lang['POPUPAUTHOR'] = 'Автор зображення';
+$lang['SHOW_ALL'] = 'Показати все';
+$lang['SHOW_COORD'] = 'Показати координати';
+$lang['VIEW_OSM'] = 'Переглянути на OpenStreetMap';
+$lang['WIDTH'] = 'Ширина мапи';
+$lang['ZOOM'] = 'Рівень масштабу';
+$lang['ZOOM_DESC'] = '1: далеко, 18: дуже близько';
+$lang['ZOOM_IN'] = 'Збільшити';
+$lang['ZOOM_OUT'] = 'Зменшити';
+$lang['ATTRPLUGIN_DESC'] = 'Я створив цей плагін. :)';
+$lang['ALWAYS'] = 'показати відразу';
+$lang['ADD_BEFORE_DESC'] = 'Де відображати інформацію про місцезнаходження.';
+$lang['ADD_BEFORE'] = 'Додати місцеросташування перед';
