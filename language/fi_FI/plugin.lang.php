7d5bc7135f702ce54707b17ec6bb2c5cdc98fd77
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-23 20:36:16 +0100

    [piwigo_openstreetmap] Update fi_FI, thanks to jhellen

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['WIDTH'] = 'Kartan leveys';
+$lang['ZOOM_IN'] = 'Lähennä';
+$lang['ZOOM_OUT'] = 'Loitonna';
+$lang['PINSHADOWPATH'] = 'Varjosus';
+$lang['PINSHADOWSIZE'] = 'Varjostuksen koko';
+$lang['M_MAP'] = 'Päävalikko';
+$lang['LINK_MAP'] = 'Linkitä tähän karttaan';
+$lang['MAP'] = 'Kartta';
+$lang['EDIT_MAP'] = 'Kartta';
+$lang['FIND_POSITION'] = 'Löydä sijaintini';
+$lang['GPX_MAP'] = 'GPX kartta';
+$lang['G_MAP'] = 'Kartan asetukset';
+$lang['HEIGHT'] = 'Kartan korkeus';
+$lang['ALWAYS'] = 'näytä heti';
+$lang['BASELAYER'] = 'Kartan tyyli';
+$lang['CENTER_MAP'] = 'Keskennä kartta tähän';
