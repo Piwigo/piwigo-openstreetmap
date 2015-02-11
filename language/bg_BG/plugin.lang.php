53923e4b58ed9b237f7a5e8bfa6107d10a378dab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-11 17:13:16 +0100

    Update bg_BG, thanks to como

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['POPUPTHUMB'] = 'Миниатюра на изображението';
+$lang['POPUPNAME'] = 'Име на изображението';
+$lang['POPUPCOMMENT'] = 'Коментар на изображението';
+$lang['POPUPAUTHOR'] = 'Автор на изображението';
+$lang['M_MAP'] = 'Главно меню';
+$lang['MAP'] = 'Карта';
+$lang['LONGITUDE'] = 'Дължина';
+$lang['LATITUDE'] = 'Ширина';
+$lang['HEIGHT_DESC'] = 'в px';
+$lang['GPX_MAP'] = 'GPX карта';
+$lang['EDIT_MAP'] = 'Карта';
+$lang['CLICKED_MAP'] = 'Вие кликнахте на картата на';
+$lang['ZOOM'] = 'Ниво на увеличение';
+$lang['ZOOM_IN'] = 'Увеличи';
+$lang['ZOOM_OUT'] = 'Намали';
+$lang['ITEMS_SCREEN'] = 'Покажи позициите от този екран';
+$lang['METERS_FROM_POINT'] = 'Вие сте в рамките на %s метра от тази точка';
+$lang['COPY_PASTE_URL'] = 'Копирайте и използвайте връзката отдолу:';
+$lang['SHARE'] = 'Начин за споделяне на този изглед';
+$lang['LINK_MAP'] = 'Връзка към тази карта';
+$lang['SEARCH_MY_POSITION'] = 'Търси се моята позиция върху картата';
+$lang['FIND_POSITION'] = 'Намери моята позиция';
+$lang['BACK_ON_PAGE'] = 'Към предишната страница';
+$lang['PIWIGO_GALLERY'] = 'Към галерията';
+$lang['ITEMS'] = '%d позиции';
+$lang['SHOW_ALL_ITEMS'] = 'Покажи всички позиции';
+$lang['SHOW_ALL_PIWIGO'] = 'Покажи всички позиции в галерията';
+$lang['SHOW_ALL'] = 'Покажи всички';
+$lang['OSM_CONTRIBUTORS'] = '<a href="http://www.openstreetmap.org" target="_blank">OpenStreetMap</a>';
+$lang['PLUGINBY'] = ':';
+$lang['PLUGIN_BY'] = '<a href="https://github.com/xbgmsharp/piwigo-openstreetmap" target="_blank">xbgmsharp</a>';
+$lang['OSWORLDMAP'] = 'Карта Open Street';
