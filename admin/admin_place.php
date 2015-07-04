4d073269156ef4f986880bd01e81b1ee65048e0d
xbgmsharp <xbgmsharp@gmail.com>
2015-07-04 11:11:44 +0100

    Add Place admin page in order to add,edit,delete #62

---
+++ admin/admin_place.php
@@ -0,0 +1,200 @@
+<?php
+/***********************************************
+* File      :   admin_place.php
+* Project   :   piwigo-openstreetmap
+* Descr     :   Create place for reuse
+*
+* Created   :   07.07.2015
+*
+* Copyright 2013-2015 <xbgmsharp@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************/
+
+// Check whether we are indeed included by Piwigo.
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+
+// Check access and exit when user status is not ok
+check_status(ACCESS_ADMINISTRATOR);
+
+// Setup plugin Language
+load_language('plugin.lang', OSM_PATH);
+
+// Fetch the template.
+global $template, $conf, $lang, $prefixeTable;
+// Easy access
+define('osm_place_table', $prefixeTable.'osm_places');
+
+/* Table to hold osm places details */
+$q = 'CREATE TABLE IF NOT EXISTS `'.osm_place_table.'` (
+                `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+                `latitude` double(8,6) NOT NULL,
+                `longitude` double(8,6) NOT NULL,
+                `name` varchar(255) DEFAULT NULL,
+                `parentId` mediumint(8),
+                PRIMARY KEY (id)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8
+        ;';
+pwg_query($q);
+
+
+// +-----------------------------------------------------------------------+
+// |                                edit places                            |
+// +-----------------------------------------------------------------------+
+
+if (isset($_POST['edit_submit']) and isset($_POST['edit_list']))
+{
+  $query = '
+SELECT name
+  FROM '.osm_place_table.'
+;';
+  $existing_names = array_from_query($query, 'name');
+
+  $current_name_of = array();
+  $query = '
+SELECT id, name, latitude, longitude
+  FROM '.osm_place_table.'
+  WHERE id IN ('.$_POST['edit_list'].')
+;';
+  $result = pwg_query($query);
+  while ($row = pwg_db_fetch_assoc($result))
+  {
+    $current_name_of[ $row['id'] ] = $row['name'];
+  }
+
+  $updates = array();
+  // we must not rename tag with an already existing name
+  foreach (explode(',', $_POST['edit_list']) as $place_id)
+  {
+    $place_name = stripslashes($_POST['place_name-'.$place_id]);
+    $place_lat = stripslashes($_POST['place_lat-'.$place_id]);
+    $place_lon = stripslashes($_POST['place_lon-'.$place_id]);
+
+    if ($place_name != $current_name_of[$place_id])
+    {
+      if (in_array($place_name, $existing_names))
+      {
+        $page['errors'][] = l10n('Place "%s" already exists', $place_name);
+      }
+      else if (!empty($place_name))
+      {
+        $updates[] = array(
+          'id' => $place_id,
+          'name' => addslashes($place_name),
+          'latitude' => $place_lat,
+          'longitude' => $place_lon,
+          );
+      }
+    }
+  }
+  mass_updates(
+    osm_place_table,
+    array(
+      'primary' => array('id'),
+      'update' => array('name', 'latitude', 'longitude'),
+      ),
+    $updates
+    );
+}
+
+// +-----------------------------------------------------------------------+
+// |                               delete places                           |
+// +-----------------------------------------------------------------------+
+
+if (isset($_POST['delete']) and isset($_POST['places']))
+{
+  $query = '
+SELECT name
+  FROM '.osm_place_table.'
+  WHERE id IN ('.implode(',', $_POST['places']).')
+;';
+  $place_names = array_from_query($query, 'name');
+
+  $query = '
+DELETE
+  FROM '.osm_place_table.'
+  WHERE id IN ('.implode(',', $_POST['places']).')
+;';
+  pwg_query($query);
+
+  $page['infos'][] = l10n_dec(
+    'The following place was deleted', 'The %d following places were deleted',
+    count($place_names)
+    )
+  .' : '.implode(', ', $place_names);
+}
+
+// +-----------------------------------------------------------------------+
+// |                               add a place                             |
+// +-----------------------------------------------------------------------+
+if (isset($_POST['add']) and !empty($_POST['add_place']))
+{
+  $query = "INSERT INTO `".osm_place_table."` (`name`, `latitude`, `longitude`) VALUE ('". $_POST['add_place'] ."', '". $_POST['add_lat'] ."', '". $_POST['add_lon'] ."');";
+  $result = pwg_query($query);
+}
+
+// all places
+$query = 'SELECT * FROM `'.osm_place_table.'`;';
+$result = pwg_query($query);
+$all_places = array();
+while ($place = pwg_db_fetch_assoc($result))
+{
+  $all_places[] = $place;
+}
+
+// Send value to templates
+$template->assign(
+	array(
+		'OSM_PATH' => OSM_PATH,
+		'all_places' => $all_places,
+	)
+);
+
+if (isset($_POST['edit']) and isset($_POST['places']))
+{
+  $list_name = 'EDIT_PLACES_LIST';
+  if (isset($_POST['duplicate']))
+  {
+    $list_name = 'DUPLIC_TAGS_LIST';
+  }
+  elseif (isset($_POST['merge']))
+  {
+    $list_name = 'MERGE_TAGS_LIST';
+  }
+
+  $template->assign($list_name, implode(',', $_POST['places']));
+
+  $query = '
+SELECT id, name, latitude, longitude
+  FROM '.osm_place_table.'
+  WHERE id IN ('.implode(',', $_POST['places']).')
+;';
+  $result = pwg_query($query);
+  while ($row = pwg_db_fetch_assoc($result))
+  {
+    $template->append(
+      'places',
+      array(
+        'ID' => $row['id'],
+        'NAME' => $row['name'],
+        'LATITUDE' => $row['latitude'],
+        'LONGITUDE' => $row['longitude'],
+        )
+      );
+  }
+}
+
+
+?>
---
+++ admin/admin_place.tpl
@@ -0,0 +1,155 @@
+{html_head}
+<link rel="stylesheet" href="{$OSM_PATH}fontello/css/osm.css" />
+{/html_head}
+
+{html_style}
+.showInfo { text-indent:5px; }
+{/html_style}
+
+Create place to allow reuse of location.
+<br/><br/>
+Refer to the <a href="https://github.com/xbgmsharp/piwigo-openstreetmap/wiki" target="_blanck">plugin documentation</a> for additional information. Create an <a href="https://github.com/xbgmsharp/piwigo-openstreetmap/issues" target="_blanck">issue</a> for support, or feedback, or feature request.
+
+{footer_script require='jquery'}
+jQuery('.showInfo').tipTip({
+  'delay' : 0,
+  'fadeIn' : 200,
+  'fadeOut' : 200,
+  'maxWidth':'300px',
+  'keepAlive':true,
+  'activation':'click'
+});
+
+function displayDeletionWarnings() {
+  jQuery(".warningDeletion").show();
+  jQuery("input[name=destination_tag]:checked").parent("label").children(".warningDeletion").hide();
+}
+
+displayDeletionWarnings();
+
+jQuery("#mergeTags label").click(function() {
+  displayDeletionWarnings();
+});
+
+jQuery("input[name=merge]").click(function() {
+  if (jQuery("ul.tagSelection input[type=checkbox]:checked").length < 2) {
+    alert("{'Select at least two places for merging'|@translate}");
+    return false;
+  }
+});
+
+$("#searchInput").on("keydown", function(e) {
+  var $this = $(this),
+      timer = $this.data("timer");
+
+  if (timer) {
+    clearTimeout(timer);
+  }
+
+  $this.data("timer", setTimeout(function() {
+    var val = $this.val();
+    if (!val) {
+      $(".tagSelection>li").show();
+      $("#filterIcon").css("visibility","hidden");
+    }
+    else {
+      $("#filterIcon").css("visibility","visible");
+      var regex = new RegExp( val.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&"), "i" );
+      $(".tagSelection>li").each(function() {
+        var $li = $(this),
+            text = $.trim( $("label", $li).text() );
+        $li.toggle(regex.test(text));
+      });
+    }
+
+  }, 300) );
+
+  if (e.keyCode == 13) { // Enter
+    e.preventDefault();
+  }
+});
+{/footer_script}
+
+<form action="" method="post" id="update">
+  {if isset($EDIT_PLACES_LIST)}
+  <fieldset>
+    <legend>{'Edit places'|@translate}</legend>
+    <input type="hidden" name="edit_list" value="{$EDIT_PLACES_LIST}">
+    <table class="table2">
+      <tr class="throw">
+        <th>{'Current'|@translate}</th>
+        <th>{'New'|@translate}</th>
+      </tr>
+      {foreach from=$places item=place}
+      <tr>
+        <td style="border-top: 1px dashed #005E89;">{$place.NAME}</td>
+        <td style="border-top: 1px dashed #005E89;"><input type="text" name="place_name-{$place.ID}" value="{$place.NAME}" size="50"></td>
+      <tr>
+      </tr>
+        <td>{$place.LATITUDE}</td>
+        <td><input type="text" name="place_lat-{$place.ID}" value="{$place.LATITUDE}" size="40"></td>
+      <tr>
+      </tr>
+        <td>{$place.LONGITUDE}</td>
+        <td><input type="text" name="place_lon-{$place.ID}" value="{$place.LONGITUDE}" size="40"></td>
+      </tr>
+      {/foreach}
+    </table>
+
+    <p>
+      <input type="submit" name="edit_submit" value="{'Submit'|@translate}">
+      <input type="submit" name="edit_cancel" value="{'Cancel'|@translate}">
+    </p>
+  </fieldset>
+  {/if}
+
+  <fieldset>
+    <legend>{'Add a place'|@translate}</legend>
+    <ul>
+	<li>
+		<label>{'New place'|@translate} : </label>
+		<input type="text" name="add_place" size="50" require="" placeholder="Home">
+	</li>
+	<li>
+		<label>{'Latitude'|@translate} : </label>
+		<input type="text" name="add_lat" size="40" require="" placeholder="48.858">
+	</li>
+	<li>
+		<label>{'Longitude'|@translate} : </label>
+		<input type="text" name="add_lon" size="40" require="" placeholder="2.2942">
+	</li>
+    </ul>
+
+    <p><input class="submit" type="submit" name="add" value="{'Submit'|@translate}"></p>
+  </fieldset>
+
+  <fieldset>
+    <legend>{'Place selection'|@translate}</legend>
+
+    {if count($all_places)}
+    <div><label><span class="icon-filter" style="visibility:hidden" id="filterIcon"></span>{'Search'|@translate}: <input id="searchInput" type="text" size="12"></label></div>
+    {/if}
+
+    <ul class="tagSelection">
+    {foreach from=$all_places item=place}
+      <li>
+        {capture name='showInfo'}{strip}
+          <b>{$place.name}</b><br>
+        {/strip}{/capture}
+        <a class="icon-info-circled-1 showInfo" title="{$smarty.capture.showInfo|@htmlspecialchars}"></a>
+        <label>
+          <input type="checkbox" name="places[]" value="{$place.id}"> {$place.name}
+        </label>
+      </li>
+    {/foreach}
+    </ul>
+
+    <p>
+      <input type="hidden" name="pwg_token" value="{$PWG_TOKEN}">
+      <input type="submit" name="edit" value="{'Edit selected places'|@translate}">
+      <input type="submit" name="duplicate" value="{'Duplicate selected places'|@translate}">
+      <input type="submit" name="delete" value="{'Delete selected places'|@translate}" onclick="return confirm('{'Are you sure?'|@translate}');">
+    </p>
+  </fieldset>
+
+</form>
