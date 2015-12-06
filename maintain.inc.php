0e8e374e9d1dbf94b8946364610f5d468a92fe61
xbgmsharp <xbgmsharp@gmail.com>
2015-12-06 10:26:49 +0000

    Make Map v3 default

--- maintain.inc.php
+++ maintain.inc.php
@@ -131,8 +131,10 @@ else if (isset(\$_GET['v']) and \$_GET['v'] == 2)
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap2.php');
 else if (isset(\$_GET['v']) and \$_GET['v'] == 3)
 	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap3.php');
+else if (isset(\$_GET['v']) and \$_GET['v'] == 4)
+	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap4.php');
 else
-	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap2.php');
+	include_once( PHPWG_ROOT_PATH. 'plugins/piwigo-openstreetmap/osmmap3.php');
 ?>
 EOF;
 	$fp = fopen( PHPWG_ROOT_PATH.'osmmap.php', 'w' );
