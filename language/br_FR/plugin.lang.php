6dae7278972712d664398466f113dcce401a734e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-02-20 07:31:35 +0100

    Update br_FR, thanks to yves.kerleguer

---
+++ language/br_FR/plugin.lang.php
@@ -0,0 +1,28 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['BASELAYER'] = 'Giz ar gartenn';
+$lang['ADD_BEFORE'] = 'Lakaat ul lec\'h a-raok';
+$lang['ADD_BEFORE_DESC'] = 'E pelec\'h diskwel titouroù al lec\'h.';
+$lang['ALWAYS'] = 'diskouez diouzhtu';
+$lang['ATTRIMAGERY'] = 'Diskouez giz ar gartenn';
+$lang['ATTRIMAGERY_DESC'] = 'Diskouez giz ar gartenn implijet.';
