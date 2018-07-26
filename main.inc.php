79d1e164b5d8b38a15680e17188bdd530ce4479f
xbgmsharp <xbgmsharp@gmail.com>
2018-07-26 11:14:06 +0200

    Bump version to 2.9.a

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: OpenStreetMap
-Version: 2.8.d
+Version: 2.9.a
 Description: OpenStreetMap integration for piwigo
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=701
 Author: xbmgsharp
