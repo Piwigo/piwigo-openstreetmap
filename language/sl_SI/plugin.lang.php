8e14e49b51434b31c8dc5512e84775453834cf9f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-15 07:06:23 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['BASELAYER'] = 'Slog zemljevida';
+$lang['G_MAP'] = 'Konfiguracija zemljevida';
+$lang['SHOWMMAP_DESC'] = 'V glavnem meniju prikažite meni z zemljevidom sveta, ki prikaže vse slike v galeriji.';
+$lang['SHOWMMAP'] = 'Dodajte zemljevid v glavni meni';
+$lang['M_MAP'] = 'Glavni meni';
+$lang['WIDTH'] = 'Širina zemljevida';
+$lang['WIDTH_DESC'] = 'v px (slikovnih pikah) ali samodejno';
+$lang['SHOWCMAP_DESC'] = 'Prikažite meni zemljevida sveta v opisu kategorije, ki prikaže vse slike v galeriji.';
+$lang['SHOWCMAP'] = 'Dodajte zemljevid v opis kategorije';
+$lang['C_MAP'] = 'Opis kategorije';
+$lang['POPUPAUTHOR'] = 'Avtor slike';
+$lang['POPUPCOMMENT'] = 'Komentar slike';
+$lang['POPUPLINK'] = 'Dodajte povezavo do slike';
+$lang['POPUPNAME'] = 'Ime slike';
+$lang['POPUPTHUMB'] = 'Sličica slike';
+$lang['LEFTPOPUPINFO'] = 'Izberite pojavno vsebino';
+$lang['LEFTPOPUPINFO_DESC'] = 'Izberite, kaj naj se prikaže v pojavnem oknu';
+$lang['NEVER'] = 'brez pojavnega okna';
+$lang['ALWAYS'] = 'prikaži takoj';
+$lang['CLICK'] = 'prikaži ob kliku';
+$lang['LEFTPOPUP'] = 'Pokaži pojavno okno';
+$lang['LEFTPOPUP_DESC'] = 'Pokažite pojavno okno, če kliknete žebljiček.';
+$lang['LEFTLINK_DESC'] = 'Vnesite ime povezave, ki se prikaže v levem meniju.';
+$lang['LEFTLINK'] = 'Ime menija zemljevida sveta';
+$lang['SHOWWORLDMAPLEFT_DESC'] = 'Prikaži meni zemljevida sveta v levem meniju, prikažejo se vse slike v galeriji.';
+$lang['SHOWWORLDMAPLEFT'] = 'Na levi dodajte meni zemljevida sveta';
+$lang['L_MAP'] = 'Konfiguracija levega menija';
+$lang['SHOWOSM_DESC'] = 'Dodajte povezavo za zemljevidom, da si ga ogledate na OpenStreetMap.';
+$lang['SHOWOSM'] = 'Prikaži \'Ogled na OpenStreetMap\'';
+$lang['RIGHTLINKCSS'] = 'CSS po meri';
+$lang['RIGHTLINKCSS_DESC'] = 'Slog CSS po meri za uporabo v glavi lokacije.';
+$lang['RIGHTLINK_DESC'] = 'Vnesite ime za prikaz na vrhu zemljevida.';
+$lang['RIGHTLINK'] = 'Ime glave lokacije';
+$lang['ZOOM'] = 'Raven povečave';
+$lang['ZOOM_DESC'] = '1: daleč, 18: zelo blizu';
+$lang['HEIGHT'] = 'Višina zemljevida';
+$lang['HEIGHT_DESC'] = 'v px (slikovnih pikah)';
+$lang['ADD_BEFORE_DESC'] = 'Kje prikazati informacije o lokaciji.';
+$lang['ADD_BEFORE'] = 'Dodajte lokacijo pred';
+$lang['SHOWLOCATION_DESC'] = 'Prikaži zemljevid na desni plošči na strani s sliko.';
+$lang['SHOWLOCATION'] = 'Prikaži zemljevid lokacije';
+$lang['DISPLAY'] = 'Konfiguracija zaslona';
+$lang['R_MAP'] = 'Konfiguracija desne plošče';
