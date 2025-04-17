c731fca7c0fd188e9f213ab33f4b00ce3f91b566
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-17 09:49:05 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Show the map on all pages of the category : '] = 'Afficher la carte sur toutes les pages de de l\'album:';
+$lang['This will display a map where the category description is'] = 'Cela affichera une carte où la description de l\'album est';
+$lang['This will show a map on all pages of a category, not just in the category\'s description'] = 'Cela affichera une carte sur toutes les pages d\'un album, pas seulement dans la description de l\'album.';
+$lang['PINOFFSET_DESC'] = 'Le décalage de la position du point d\'ancrage de l\'image de l\'épingle, par rapport au coin haut-gauche. 0x0 est le coin haut-gauche. Exemple d\'utilisation : croix (réticule) = centrer sur l\'image 16x16 (cf. illustration b)';
+$lang['PINOFFSET'] = 'Décalage de l\'épingle';
+$lang['PINPOPUPOFFSET'] = 'décalage du Popup';
+$lang['DISPLAY_GPX_FILE'] = 'Sélectionnez un fichier GPX à afficher sur toutes les cartes de description des albums';
+$lang['Display map on category page'] = 'Afficher la carte sur la page des albums';
+$lang['CUSTOMBASELAYERURL'] = 'URL du serveur de tuile';
+$lang['ATTRLEAFLET'] = '\'Propulsé par Leaflet\'';
