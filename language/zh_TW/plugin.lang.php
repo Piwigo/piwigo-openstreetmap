b5641f3ca28f878e00f10e0b2b1a644ffb5fbd1d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-09 06:26:22 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d geotagged items in your gallery'] = '藝廊中有 %d 個加上地理標籤的項目';
+$lang['ADD_BEFORE'] = '在前面加上位置';
+$lang['ADD_BEFORE_DESC'] = '要在何處顯示位置資訊。';
+$lang['ALWAYS'] = '立即顯示';
+$lang['ATTRIMAGERY'] = '顯示地圖樣式';
+$lang['ATTRIMAGERY_DESC'] = '顯示使用過的地圖樣式';
+$lang['ATTRLEAFLET'] = '顯示函式庫說明';
+$lang['ATTRLEAFLET_DESC'] = '顯示互動式 \'Leaflet\'地圖所使用的 JavaScript 函式庫的屬性。';
+$lang['ATTRPLUGIN'] = '顯示作者說明';
+$lang['ATTRPLUGIN_DESC'] = '我創造了這個插件。 :)';
+$lang['AUTO_CENTER'] = '自動置中';
+$lang['AUTO_CENTER_HELP'] = '地圖會自動置中、縮放至可包含所有的資訊。';
+$lang['Add map to community batch manager :'] = '將地圖加入社群批次管理員：';
+$lang['BACK_ON_PAGE'] = '前往前一頁';
+$lang['BASELAYER'] = '地圖樣式';
+$lang['BASELAYER_DESC'] = 'OpenStreetMap 的預設樣式為 Mapnik。';
+$lang['BROWSER_JAVASCRIPT'] = '您必須開啟瀏覽器的 JavaScript 功能';
+$lang['CENTER_MAP'] = '將此處設定為地圖中心';
+$lang['CENTER_MAP_DESC'] = '將世界地圖置中於特定位置（GPS 座標）。緯度和經度以逗號分隔，預設為 0,0';
+$lang['CITY'] = '城市';
